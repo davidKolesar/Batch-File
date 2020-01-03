2551489773999a6d7aed9c3d17eec232ea207ef8
unknown <djkolesa@gmail.com>
2020-01-02 20:47:10 -0800

    Adding GoTos

--- tools.bat
+++ tools.bat
@@ -3,8 +3,22 @@ title Scrub Tools
 
 cls
 echo ***********************************************************
-echo 				  WINDOWS 10 SCRUB
-echo 					David Kolesar
+echo WINDOWS 10 SCRUB
+echo David Kolesar
 echo ***********************************************************
-echo 
+echo .  
+echo Select Option By Number:
+echo.
+echo 1) Option
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto:1
 
+:1
+echo hit 1
+set input=
+if %input%==2 goto:2
+:2
+echo hit 2
+exit
