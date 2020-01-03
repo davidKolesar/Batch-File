b653c687663ef2ef9cb45deedc63f4d3b6e3798e
unknown <djkolesa@gmail.com>
2020-01-02 20:52:55 -0800

    Adding GoTos

--- tools.bat
+++ tools.bat
@@ -11,14 +11,19 @@ echo Select Option By Number:
 echo.
 echo 1) Option
 echo 2) Exit
+
+:Start
 set input=
 set /p input= Choice:
-if %input%==1 goto:1
+if %input%==1 goto :Option 1
+if %input%==2 goto: Exit
 
-:1
-echo hit 1
-set input=
-if %input%==2 goto:2
-:2
-echo hit 2
+:Option 1
+echo option 1
+notepad.exe
+goto : Start
+
+
+:Exit
+echo Goodbye. 
 exit
