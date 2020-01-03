4d54673a1f25e1ebfeccb0bb19e5dc78a0822680
unknown <djkolesa@gmail.com>
2020-01-02 22:06:51 -0800

    removed functionality

--- computerInformation.bat
+++ computerInformation.bat
@@ -11,15 +11,12 @@ echo .
 echo Select Option By Number:
 echo.
 echo 1) Generate System and Network Information File
-echo 2) Generate System and Network Information to console
-echo 3) Exit
+echo 2) Exit
 
 :Start
-set input=
-set /p input= Choice:
-if %input%==1 goto :Option 1
-if %input%==2 goto :Option 2
-if %input%==3 goto :Exit
+set /p choice= 
+if %choice%==1 GOTO Option 1
+if %choice%==2 GOTO Exit
 
 :Option 1
 echo Generating File
@@ -32,7 +29,6 @@ echo Network Properties :
 ipconfig /all
 echo Network Connections : 
 netstat
-
 echo Date:
 echo %DATE%
 echo Time: 
@@ -40,22 +36,6 @@ echo %TIME%
 )
 cls
 goto :Program Options
-
-:Option 2
-echo Computer Name :
-hostname
-echo System Information:
-systeminfo 
-echo Network Properties : 
-ipconfig /all
-echo Network Connections : 
-netstat
-echo Date:
-echo %DATE%
-echo Time: 
-echo %TIME%
-goto :Program Options
-
 
 :Exit
 echo Goodbye
