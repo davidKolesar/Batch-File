f8b7e2006f0c031e47fdd1f045f1493e560c8519
unknown <djkolesa@gmail.com>
2020-01-02 21:54:52 -0800

    fixing ANSI bug

--- computerInformation.bat
+++ computerInformation.bat
@@ -10,15 +10,15 @@ echo ***********************************************************
 echo .  
 echo Select Option By Number:
 echo.
-echo 1) Generate System Information File
-echo 2)
+echo 1) Generate System and Network Information File
+echo 2) Generate System and Network Information to console
 echo 3) Exit
 
 :Start
 set input=
 set /p input= Choice:
 if %input%==1 goto :Option 1
-if %input%==2 goto :Exit
+if %input%==2 goto :Option 2
 if %input%==3 goto :Exit
 
 :Option 1
@@ -30,6 +30,9 @@ echo System Information:
 systeminfo 
 echo Network Properties : 
 ipconfig /all
+echo Network Connections : 
+netstat
+
 echo Date:
 echo %DATE%
 echo Time: 
@@ -38,6 +41,20 @@ echo %TIME%
 goto :Program Options
 
 :Option 2
+
+echo Computer Name :
+hostname
+echo System Information:
+systeminfo 
+echo Network Properties : 
+ipconfig /all
+echo Network Connections : 
+netstat
+
+echo Date:
+echo %DATE%
+echo Time: 
+echo %TIME%
 goto :Program Options
 
 
