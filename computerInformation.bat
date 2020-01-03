faf3476621451e6411448babc1d786caad3a1f23
unknown <djkolesa@gmail.com>
2020-01-02 21:29:51 -0800

    Adding 3rd option

--- computerInformation.bat
+++ computerInformation.bat
@@ -10,14 +10,16 @@ echo ***********************************************************
 echo .  
 echo Select Option By Number:
 echo.
-echo 1) Generate System Information
-echo 2) Exit
+echo 1) Generate System Information File
+echo 2)
+echo 3) Exit
 
 :Start
 set input=
 set /p input= Choice:
 if %input%==1 goto :Option 1
 if %input%==2 goto :Exit
+if %input%==3 goto :Exit
 
 :Option 1
 echo Generating File
@@ -33,6 +35,9 @@ echo %DATE%
 echo Time: 
 echo %TIME%
 )
+goto :Program Options
+
+:Option 2
 goto :Program Options
 
 
